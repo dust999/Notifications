0328a74f13161d3e79ff183b35dcbc88373afdd9
Levon Tumasov <dust999@gmail.com>
2025-06-29 22:16:47 +0500

    Updated configs

--- utils.py
+++ utils.py
@@ -1,17 +1,15 @@
 import json
 import os
 
-def load_json(file_path, default_value):
-    """Load JSON data from a file, returning default_value if the file doesn't exist or is invalid."""
-    if os.path.exists(file_path) and os.path.getsize(file_path) > 0:
-        try:
-            with open(file_path, 'r') as f:
-                return json.load(f)
-        except json.JSONDecodeError:
-            return default_value
-    return default_value
+def load_json(file_path, default=None):
+    """Load data from a JSON file with UTF-8 encoding."""
+    try:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            return json.load(f)
+    except Exception:
+        return default if default is not None else []
 
 def save_json(file_path, data):
-    """Save data to a JSON file with pretty printing."""
-    with open(file_path, 'w') as f:
+    """Save data to a JSON file with pretty printing and UTF-8 encoding."""
+    with open(file_path, 'w', encoding='utf-8') as f:
         json.dump(data, f, indent=4, ensure_ascii=False)
